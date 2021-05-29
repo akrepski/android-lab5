bbf4ad019753a3b5f3c96ad17fcc6ecfc6de5001
akrepski <ilpiqs@gmail.com>
2021-05-29 12:48:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android_lab5/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android_lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] values = new String[] {"kot", "pies", "łoś", "zając", "ryś"};
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listView = (ListView) findViewById((R.id.listView));
+        listView.setAdapter(this.adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intent = new Intent(this, DodajWpis.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+}
